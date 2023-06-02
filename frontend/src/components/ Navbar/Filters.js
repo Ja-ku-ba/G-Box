2cdef20f98db43db4c00cc33bda54f01d863e2c7
Kuba <wulfrykpierozek@gmail.com>
2023-06-02 21:55:56 +0200

    nav bar improvments

--- frontend/src/components/ Navbar/Filters.js
+++ frontend/src/components/ Navbar/Filters.js
@@ -4,49 +4,70 @@ function Filters() {
       <li>
         <ul>
           <label for={"from-messages"}>Od</label>
-          <input type={"text"} id={"from-messages"} />
+          <div className="inputs-parent">
+            <input type={"text"} id={"from-messages"} />
+          </div>
         </ul>
         <ul>
           <label htmlFor={"to-messages"}>Do</label>
-          <input type={"text"} id={"to-messages"} />
+          <div className="inputs-parent">
+            <input type={"text"} id={"to-messages"} />
+          </div>
         </ul>
         <ul>
           <label htmlFor={"topic-messages"}>Temat</label>
-          <input type={"text"} id={"topic-messages"} />
+          <div className="inputs-parent">
+            <input type={"text"} id={"topic-messages"} />
+          </div>
         </ul>
         <ul>
           <label htmlFor={"contains-messages"}>Zawiera słowa</label>
-          <input type={"text"} id={"contains-messages"} />
+          <div className="inputs-parent">
+            <input type={"text"} id={"contains-messages"} />
+          </div>
         </ul>
         <ul>
           <label htmlFor={"exclude-messages"}>Nie zawiera</label>
-          <input type={"text"} id={"exclude-messages"} />
-        </ul>
-        <ul>
-          <label htmlFor={"size-messages"}>Rozmiar</label>
-          <input type={"text"} id={"size-messages"} />
-          <div className="custom-select">
-            <select>
-              <option value="0">Kb</option>
-              <option value="1">Mb</option>
-              <option value="2">Gb</option>
-            </select>
+          <div className="inputs-parent">
+            <input type={"text"} id={"exclude-messages"} />
           </div>
         </ul>
         <ul>
-          <label htmlFor={"date-messages"}>Zakres dat</label>
-          <div>
-            <label htmlFor="birthday">Od:</label>
-            <input type="date" id="birthday" name="birthday" />
+          <div className="filters-date-aligment">
+            <label htmlFor={"size-messages"}>Rozmiar</label>
+            <div className="inputs-parent">
+              <input type={"number"} id={"size-messages"} />
+              <select>
+                <option value="0">Mniej niż</option>
+                <option value="1">Więcej niż</option>
+                <option value="2">Tyle samo</option>
+              </select>
+              <select>
+                <option value="0">Kb</option>
+                <option value="1">Mb</option>
+                <option value="2">Gb</option>
+              </select>
+            </div>
           </div>
-          <div>
-            <label htmlFor="birthday">Do:</label>
-            <input type="date" id="birthday" name="birthday" />
-          </div>
-          <input type={"text"} id={"date-messages"} />
         </ul>
+        {/*<ul>*/}
+        {/*  <label htmlFor={"date-messages"}>Zakres dat</label>*/}
+        {/*  <div>*/}
+        {/*    <label htmlFor="birthday">Od:</label>*/}
+        {/*    <input type="date" id="birthday" name="birthday" />*/}
+        {/*  </div>*/}
+        {/*  <div>*/}
+        {/*    <label htmlFor="birthday">Do:</label>*/}
+        {/*    <input type="date" id="birthday" name="birthday" />*/}
+        {/*  </div>*/}
+        {/*  <input type={"text"} id={"date-messages"} />*/}
+        {/*</ul>*/}
       </li>
-      <input type={"submit"} placeholder="Szukaj" />
+      <input
+        className={"search-bar-submit"}
+        type={"submit"}
+        placeholder="Szukaj"
+      />
     </form>
   );
 }
