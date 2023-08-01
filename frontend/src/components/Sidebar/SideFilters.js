640da64ce9cb380e707d7437ff6e5c96473cd81b
Kuba kuba <wulfrykpierozek@gmail.com>
2023-08-01 17:10:28 +0200

    sidebar elements redirects to mailviwe with proper filter

--- frontend/src/components/Sidebar/SideFilters.js
+++ frontend/src/components/Sidebar/SideFilters.js
@@ -1,13 +1,19 @@
+import { useNavigate } from "react-router-dom"
+
 const SideFilters = () => {
+  const navigate = useNavigate()
+  const redirect = (filter) => {
+    navigate(`/${filter.target.id}`)
+  }
   return (
     <div className="sidebar">
       <ul>
-        <li>Odebrane</li>
-        <li>Oznaczone gwiazdką</li>
-        <li>Wysłane</li>
-        <li>Wersje robocze</li>
-        <li>Kosz</li>
-        <li>Spam</li>
+        <li onClick={(e) => redirect(e)} id="ALL">Odebrane</li>
+        <li onClick={e => redirect(e)} id="FLAGGED">Oznaczone gwiazdką</li>
+        <li onClick={e => redirect(e)} id="SENT">Wysłane</li>
+        <li onClick={e => redirect(e)} id="DRAFT">Wersje robocze</li>
+        <li onClick={e => redirect(e)} id="DELETED">Kosz</li>
+        <li onClick={e => redirect(e)} id="SPAM">Spam</li>
       </ul>
     </div>
   );
